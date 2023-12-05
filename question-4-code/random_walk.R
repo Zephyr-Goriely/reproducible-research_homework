32466ec206426470ec852009cab2441bdbb0fe80
Zephyr Goriely <YOUR_EMAIL>
2023-12-05 15:23:29 +0000

    changes

--- question-4-code/random_walk.R
+++ question-4-code/random_walk.R
@@ -1,11 +1,14 @@
-#install.packages("ggplot2")
-#install.packages("gridExtra")
+#Install and Loading in the required packages
+
+install.packages("ggplot2")
+install.packages("gridExtra")
 
 library(ggplot2)
 library(gridExtra)
 
-random_walk  <- function (n_steps) {
-  
+#Code to define the random walk function, adjusted to allow for random seeds to be set. This is done by adjusting the function with 'set.seed' function which sets the seed of objects defined with this function to whatever number is designated to 'seed'
+random_walk  <- function (n_steps, seed = NULL) {
+  set.seed(seed)
   df <- data.frame(x = rep(NA, n_steps), y = rep(NA, n_steps), time = 1:n_steps)
   
   df[1,] <- c(0,0,1)
@@ -28,7 +31,8 @@ random_walk  <- function (n_steps) {
   
 }
 
-data1 <- random_walk(500)
+# Setting the seed for the first brownian motion under the term 'data1 with seed = 33' and defining the graph as 'plot1' to visualise the random behaviour:
+data1 <- random_walk(500, seed = 33)
 
 plot1 <- ggplot(aes(x = x, y = y), data = data1) +
   
@@ -40,7 +44,8 @@ plot1 <- ggplot(aes(x = x, y = y), data = data1) +
   
   ylab("y-coordinate")
 
-data2 <- random_walk(500)
+#The previous step is repeated with a new seed 'data2', this time set with a seed 52:
+data2 <- random_walk(500, seed = 52)
 
 plot2 <- ggplot(aes(x = x, y = y), data = data2) +
   
@@ -52,4 +57,5 @@ plot2 <- ggplot(aes(x = x, y = y), data = data2) +
   
   ylab("y-coordinate")
 
+# Finally, the graphs are plotted beside each other so that the different behaviours can be compared using the code below:
 grid.arrange(plot1, plot2, ncol=2)
